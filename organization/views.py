1da5684507e254e8d60d8e898e558e3e7a6c9291
KhadeejaDin <khadeejadin@gmail.com>
2016-11-09 19:32:15 -0500

    Create templates folder for organization app

--- organization/views.py
+++ organization/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>Organization Page</h2>")
+    return render(request, 'organization/home.html')
