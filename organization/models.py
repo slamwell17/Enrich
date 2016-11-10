8d7b72a1286a23e65e00ba711e08f81ab44f380b
KhadeejaDin <khadeejadin@gmail.com>
2016-11-09 20:24:48 -0500

    Set up models for organization app

--- organization/models.py
+++ organization/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Organization(models.Model):
+    organization_id = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    free = models.BooleanField()
+    tution = models.TextField()
+    rating = models.DecimalField(max_digits = 2, decimal_places = 1)
+    category = models.TextField()
+    address = models.TextField()
