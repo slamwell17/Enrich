25cda86463bea145fa6440a11bd51d118cc12c4f
Alan Chen <alanc1995@gmail.com>
2016-11-09 21:19:05 -0500

    base templating

--- Enrich/urls.py
+++ Enrich/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^organization/', include('organization.urls')),
+    url(r'^search/', include('search.urls')),
 ]
--- search/views.py
+++ search/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>SEARCH PAGE</h2>")
+    return render(request, 'search/search_results.html')
